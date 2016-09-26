68770c46de14fefe2cfa37afb52c7da8a48f7528
follow-my-heart <874204896@qq.com>
2016-09-26 17:52:25 +0800

    21求和~

--- main.js
+++ main.js
@@ -142,3 +142,8 @@ function partial(fn, str1, str2) {
      }
      return result;
  }
+//21、函数 useArguments 可以接收 1 个及以上的参数。请实现函数 useArguments，返回所有调用参数相加后的结果。本题参数全部为 Number 类型，不需考虑参数转换
+function useArguments() {
+    var arr=Array.prototype.slice.call(arguments)//把arguments类数组转化为数组
+    return eval(arr.join("+"));//求和
+}
